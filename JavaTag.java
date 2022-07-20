e7ae3865f7d26f571f4f8f85c52575ed7820deb4
Robatortas <72624799+Robatortas@users.noreply.github.com>
2022-07-20 08:09:39 -0800

    Update JavaTag.java

--- JavaTag.java
+++ JavaTag.java
@@ -1,5 +1,6 @@
 class JavaTag {
   public static void main(String[] args){
+    // This code doesn't even work, I just made this so this repo counts as a java project! (Which it is)
       System.out.println("Java Tag!");
   }
 }
